992a5d223ce143289b852a3dd29ca35d89adf2e3
Pramod Tiwari <you@tpromode@gmai.com>
2018-08-09 18:28:38 +0545

    pointfield

--- hydropower/core/api/viewset.py
+++ hydropower/core/api/viewset.py
@@ -1,13 +1,14 @@
 from rest_framework import viewsets, serializers
 from rest_framework.response import Response
-from django.core.serializers import serialize
 import json
+from django.core.serializers import serialize
 from core.models import Hydropower
 from .serializers import HydropowerSerializer
 
 class HydropowerViewSet(viewsets.ModelViewSet):
-	# queryset = Hydropower.objects.all()
-	# serializer_class = HydropowerSerializer
-    return Response(json.loads(serialize('geojson', Hydropower.objects.all(),
+    # queryset = Hydropower.objects.all()
+    # serializer_class = HydropowerSerializer
+    def get(self, request, normal=None):
+        return Response(json.loads(serialize('geojson', Hydropower.objects.all(),
               geometry_field='point',
               fields=('name',))))
