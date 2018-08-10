96019442e93bdc98df05cefd0fc9ed4732acf7ac
Pramod Tiwari <you@tpromode@gmai.com>
2018-08-10 10:51:28 +0545

    update hydropower

--- hydropower/hydro/local_settings_sample.py
+++ hydropower/hydro/local_settings_sample.py
@@ -2,7 +2,7 @@ from .settings import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'hydro',
         'USER': 'postgres',
         'PASSWORD': '',
