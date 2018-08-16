ccdf7a6af7fa83b265b024efec76dd6d9e7d0c70
Pramod Tiwari <tpromode@gmail.com>
2018-08-16 13:38:00 +0545

    update

--- hydropower/hydro/urls.py
+++ hydropower/hydro/urls.py
@@ -17,11 +17,11 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
-if settings.DEBUG:
-    import debug_toolbar
+# if settings.DEBUG:
+#     import debug_toolbar
 urlpatterns = [
 	path('core/', include('core.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('core.api.urls')),
-    path('^__debug__/', include(debug_toolbar.urls)),
+    # path('^__debug__/', include(debug_toolbar.urls)),
 ]
