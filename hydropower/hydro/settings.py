69ce97e629c5c8b90c8880bb27d4b331e923ae86
Pramod Tiwari <tpromode@gmail.com>
2018-08-20 15:44:58 +0545

    settings update

--- hydropower/hydro/settings.py
+++ hydropower/hydro/settings.py
@@ -125,6 +125,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
--- hydropower/hydro/urls.py
+++ hydropower/hydro/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 
 # if settings.DEBUG:
 #     import debug_toolbar
@@ -25,3 +26,6 @@ urlpatterns = [
     path('api/', include('core.api.urls')),
     # path('^__debug__/', include(debug_toolbar.urls)),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
